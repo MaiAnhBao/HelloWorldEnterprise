18dec15bdb163e6f322619e4f752ac0f76dd2d35
Mai Anh Bao <chuyendungdangki@yahoo.com>
2017-04-17 16:09:08 +0700

    main

---
+++ main.cpp
@@ -0,0 +1,6 @@
+#include "includes/HelloWorldEnterprise.h"
+
+int main(int argc, char **argv) {
+	DesignPatterns::Enterprise::HelloWorldEnterprise::instantiateHelloWorldMainClassAndRun();
+	return 0;
+}
